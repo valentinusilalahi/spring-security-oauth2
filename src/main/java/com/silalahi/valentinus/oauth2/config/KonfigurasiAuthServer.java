25fa694af9f58e3ff543bdc36e36a8e2510acc7f
valentinusilalahi <valentinus.silalahi@gmail.com>
2019-04-13 01:15:26 +0700

    enable refresh token

--- src/main/java/com/silalahi/valentinus/oauth2/config/KonfigurasiAuthServer.java
+++ src/main/java/com/silalahi/valentinus/oauth2/config/KonfigurasiAuthServer.java
@@ -1,14 +1,27 @@
 package com.silalahi.valentinus.oauth2.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 
 @Configuration
 @EnableAuthorizationServer
 public class KonfigurasiAuthServer extends AuthorizationServerConfigurerAdapter {
+
+    @Autowired
+    @Qualifier("authenticationManagerBean")
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    @Qualifier("userDetailsServiceBean")
+    private UserDetailsService userDetailsService;
 
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
@@ -17,12 +30,18 @@ public class KonfigurasiAuthServer extends AuthorizationServerConfigurerAdapter 
                 .authorities("APP_CLIENT_OAUTH2")
                 .secret("aplikasi123")
                 .authorizedGrantTypes("authorization_code", "refresh_token")
-                .scopes("modul-transaksi", "modul-notify");
+                .scopes("modul-transaksi", "modul-notify")
+                .accessTokenValiditySeconds(180);
     }
 
     @Override
     public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
         security.checkTokenAccess("hasAuthority('APP_CLIENT_OAUTH2')");
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.authenticationManager(authenticationManager).userDetailsService(userDetailsService);
     }
 
 }
--- src/main/java/com/silalahi/valentinus/oauth2/config/SecurityConfig.java
+++ src/main/java/com/silalahi/valentinus/oauth2/config/SecurityConfig.java
@@ -2,10 +2,12 @@ package com.silalahi.valentinus.oauth2.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -59,5 +61,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and().logout()
                 .and().authorizeRequests().antMatchers("/public.html").permitAll()
                 .and().authorizeRequests().anyRequest().authenticated();
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    @Bean
+    public UserDetailsService userDetailsServiceBean() throws Exception{
+        return super.userDetailsServiceBean();
     }
 }

cb2c45d099ff6cc8fba6896f154bb4832eb9e680
valentinusilalahi <valentinus.silalahi@gmail.com>
2019-04-12 23:53:25 +0700

    spring security standard

--- src/main/java/com/silalahi/valentinus/oauth2/SpringSecurityOauth2Application.java
+++ src/main/java/com/silalahi/valentinus/oauth2/SpringSecurityOauth2Application.java
@@ -2,12 +2,18 @@ package com.silalahi.valentinus.oauth2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
+@Controller
 public class SpringSecurityOauth2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSecurityOauth2Application.class, args);
 	}
+
+	@GetMapping("/protected")
+	public void protectedPage() {}
 
 }
--- src/main/java/com/silalahi/valentinus/oauth2/config/SecurityConfig.java
+++ src/main/java/com/silalahi/valentinus/oauth2/config/SecurityConfig.java
@@ -13,33 +13,51 @@ import javax.sql.DataSource;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    private static final String SQL_LOGIN = "select u.username, up.password, u.active from s_users_passwords up inner join s_users u on u.id = up.id_user where u.username = ?";
-    private static final String SQL_PERMISSION = "select u.username, p.value as authority from s_users u inner join s_roles r on r.id = u.id_role inner join s_roles_permissions rp on rp.id_role = r.id inner join s_permissions p on rp.id_permission = p.id where u.username = ?";
+//    private static final String SQL_LOGIN = "select u.username, up.password, u.active from s_users_passwords up inner join s_users u on u.id = up.id_user where u.username = ?";
+//    private static final String SQL_PERMISSION = "select u.username, p.value as authority from s_users u inner join s_roles r on r.id = u.id_role inner join s_roles_permissions rp on rp.id_role = r.id inner join s_permissions p on rp.id_permission = p.id where u.username = ?";
+//
+//    @Autowired
+//    private DataSource dataSource;
+//
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.jdbcAuthentication()
+//                .dataSource(dataSource)
+//                .usersByUsernameQuery(SQL_LOGIN)
+//                .authoritiesByUsernameQuery(SQL_PERMISSION)
+//                .passwordEncoder(passwordEncoder());
+//    }
+//
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.authorizeRequests()
+//                .anyRequest().authenticated()
+//                .and()
+//                .formLogin()
+//                .permitAll()
+//                .defaultSuccessUrl("/home");
+//    }
 
-    @Autowired
-    private DataSource dataSource;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.jdbcAuthentication()
-                .dataSource(dataSource)
-                .usersByUsernameQuery(SQL_LOGIN)
-                .authoritiesByUsernameQuery(SQL_PERMISSION)
-                .passwordEncoder(passwordEncoder());
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        auth.inMemoryAuthentication()
+                .withUser("vale")
+                .authorities("USER_VIEW")
+                .password("silalahi");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .anyRequest().authenticated()
-                .and()
-                .formLogin()
-                .permitAll()
-                .defaultSuccessUrl("/home");
+        http
+                .formLogin().permitAll()
+                .and().logout()
+                .and().authorizeRequests().antMatchers("/public.html").permitAll()
+                .and().authorizeRequests().anyRequest().authenticated();
     }
 }
